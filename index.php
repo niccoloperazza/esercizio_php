ad76092d7549517cd8375ffd46cfe6009c8fb2eb
NiccoloPerazza <niccolo@videeco.com>
2022-03-29 12:04:02 +0200

    Inserito bottone di modifica

--- index.php
+++ index.php
@@ -14,7 +14,9 @@ $connection->close();
 
 while ( $row = $result->fetch_assoc() ) {
     // il mio codice andrà qui
-    echo "<b>" . $row["nome"] . "</b> ha l'id numero: " . $row["id"] . "<br>";
+    echo "<b>" . $row["nome"] . "</b> ha l'id numero: " . $row["id"] . "";
+    echo " <a href='modifica.php'>modifica</a>";
+    echo "<br>";
 }
 
 
